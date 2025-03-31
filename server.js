476b3ca89d18aa1d0d0644f3e59edbf2821c959c
aditya <aditya.rouss@gmail.com>
2025-04-01 01:54:00 +0530

    removing port from server.js

--- server.js
+++ server.js
@@ -63,6 +63,6 @@ app.post("/getColleges", (req, res) => {
 
 module.exports = app;
 // Start server
-app.listen(5000, () => {
-    console.log("Server is running on port 5000");
-});
+// app.listen(5000, () => {
+//     console.log("Server is running on port 5000");
+// });
